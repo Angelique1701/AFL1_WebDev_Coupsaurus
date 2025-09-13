7c5af6744466e9bd4e17022ea492ba5325b2638b
Angelique1701 <angeliquekw50@gmail.com>
2025-09-13 22:58:17 +0700

    .

--- view_AgencyList.php
+++ view_AgencyList.php
@@ -1,6 +1,8 @@
 <div class="d-flex justify-content-between align-items-center mb-3">
     <h1 class="h3 mb-0">Agency List</h1>
-    <a href="add_agency.php" class="btn btn-primary">Add New Agency</a>
+    <a href="view_AgencyAdd.php" class="btn btn-primary">Add New Agency</a>
+    <a href="view_AgencyDelete.php" class="btn btn-primary">Delete Agency</a>
+    <a href="view_AgencyUpdate.php" class="btn btn-primary">Update Agency</a>
 </div>
 
 <table class="table table-bordered">
